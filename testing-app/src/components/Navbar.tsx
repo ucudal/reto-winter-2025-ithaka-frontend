8c467fec071b644bae43a5353a69bb6b0df48649
Agus1441 <agustin.schlechter@correo.ucu.ed>
2025-07-30 19:09:13 -0300

    Transition application from React to Next.js framework.

--- testing-app/src/components/Navbar.tsx
+++ testing-app/src/components/Navbar.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
 export const Navbar = () => (
   <header className="navbar">
     <div className="navbar-container">
-      <Link to="/" className="navbar-title">Fake Ithaka</Link>
+      <Link href="/" className="navbar-title">Fake Ithaka</Link>
       <nav className="navbar-nav">
-        <Link to="/">Inicio</Link>
-        <Link to="/#uso">Uso</Link>
-        <Link to="/faq">Preguntas Frecuentes</Link>
+        <Link href="/">Inicio</Link>
+        <Link href="/faq">Preguntas Frecuentes</Link>
       </nav>
     </div>
   </header>
