06a4e5825113cf986a114c8bb6acd63ddeef3992
FausUCU <88690038+FausUCU@users.noreply.github.com>
2025-07-31 19:35:10 -0300

    Remove CopilotChat

--- src/app/components/ChatPopup.tsx
+++ src/app/components/ChatPopup.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { CopilotKitCSSProperties, CopilotPopup } from "@copilotkit/react-ui";
 
+
 export default function ChatPopup() {
   const POPUP_THEME_COLOR = "deepskyblue";
   return (
@@ -13,8 +14,11 @@ export default function ChatPopup() {
       labels={{
         title: 'Asistente de ITHAKA',
         initial: 'Hola soy el asistente de ITHAKA, ¿en qué puedo ayudarte hoy?',
+
+        
       }}
     >
+    
       </CopilotPopup>
     </div>
   );
