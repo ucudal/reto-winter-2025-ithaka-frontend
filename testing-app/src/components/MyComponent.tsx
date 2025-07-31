dca2541fe0aabed7889ac379aa033aa382ce454d
Agus1441 <141652564+Agus1441@users.noreply.github.com>
2025-07-31 19:54:48 -0300

    Update MyComponent.tsx

--- testing-app/src/components/MyComponent.tsx
+++ testing-app/src/components/MyComponent.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-interface MyComponent {
-  text: string;
-}
+interface MyComponentProps {
+   text: string;
+ }
 
-export const MyComponent: React.FC<MyComponent> = ({ text }) => (
+export const MyComponent: React.FC<MyComponentProps> = ({ text }) => (
   <div className="mi-componente">
     <p className="mi-componente-text">{text}</p>
   </div>
