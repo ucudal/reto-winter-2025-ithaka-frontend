6ad00cebdeb3ef46cf6d38b2e0a1bd2f5bd7313a
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2025-08-04 22:27:10 -0300

    rebuild component + use process env for copilot api key

--- testing-app/src/pages/HomePage.tsx
+++ testing-app/src/pages/HomePage.tsx
@@ -19,7 +19,7 @@ export const HomePage = () => {
         <SectionContact />
       </main>
       <Footer />
-      <ChatPopup />
+      <ChatPopup publicApiKey={process.env.NEXT_PUBLIC_COPILOT_KEY}/>
     </div>
   );
 };
